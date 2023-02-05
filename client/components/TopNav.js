278d9a614a187aa284d21222d8d70e3d813309a0
parisadikshit <2018bec034@sggs.ac.in>
2023-02-05 20:06:46 +0530

    Added logout function and api call to the backend

--- client/components/TopNav.js
+++ client/components/TopNav.js
@@ -1,43 +1,78 @@
-import {Menu} from 'antd';
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
-import {AppstoreOutlined, LoginOutlined, UserAddOutlined } from '@ant-design/icons';
-import Login from '../pages/login';
-const {Item} = Menu;
-const TopNav = () =>{
-    const [current, setCurrent] = useState(" ")
+import { Menu } from "antd";
+import Link from "next/link";
+import { useState, useEffect, useContext } from "react";
+import {
+  AppstoreOutlined,
+  LoginOutlined,
+  UserAddOutlined,
+  CoffeeOutlined,
+} from "@ant-design/icons";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-    useEffect(()=>{
-        
-        if(typeof window){
-            console.log(window.location.pathname)
-            setCurrent(window.location.pathname)
-        }
-        
-    },[typeof window])
-    console.log('current',current);
-    return (
-        <>
-        <Menu mode='horizontal center' selectedKeys={[current]}>
-            <Item key='/' onClick={(e)=>setCurrent(e.key)} icon={<AppstoreOutlined/>}>
-                <Link href='/'>
-                    App
-                </Link>
-            </Item>
-            <Item key='/login' onClick={(e)=>setCurrent(e.key)} icon={<LoginOutlined/>}>
-                <Link href="/login">
-                    Login
-                </Link>
-            </Item>
-            <Item key='/register' onClick={(e)=>setCurrent(e.key)} icon={<UserAddOutlined/>}>
-                <Link href="/register">
-                    Register
-                </Link>
-            </Item>
-        </Menu>
-        </>
-    );
+const { Item, SubMenu } = Menu;
+const TopNav = () => {
+  const [current, setCurrent] = useState(" ");
+  const { state, dispatch } = useContext(Context);
+  const router = useRouter();
+  const { user } = state;
+  useEffect(() => {
+    if (typeof window) {
+      console.log(window.location.pathname);
+      setCurrent(window.location.pathname);
     }
-
+  }, [typeof window]);
+  const logout = async () => {
+    dispatch({ type: "LOGOUT" });
+    window.localStorage.removeItem("user");
+    const { data } = await axios.get("/api/logout");
+    toast(data.message);
+    router.push("/login");
+  };
+  return (
+    <>
+      <Menu mode="horizontal center" selectedKeys={[current]}>
+        <Item
+          key="/"
+          onClick={(e) => setCurrent(e.key)}
+          icon={<AppstoreOutlined />}
+        >
+          <Link href="/">App</Link>
+        </Item>
+        {user == null && (
+          <>
+            <Item
+              key="/login"
+              onClick={(e) => setCurrent(e.key)}
+              icon={<LoginOutlined />}
+            >
+              <Link href="/login">Login</Link>
+            </Item>
+            <Item
+              key="/register"
+              onClick={(e) => setCurrent(e.key)}
+              icon={<UserAddOutlined />}
+            >
+              <Link href="/register">Register</Link>
+            </Item>
+          </>
+        )}
+        {user !== null && (
+        //   <SubMenu icon={<CoffeeOutlined />} title={user && user.name} className="float-right">
+            <Item
+              onClick={logout}
+              icon={<LoginOutlined />}
+              className="float-right"
+            >
+              Log out
+            </Item>
+        //   </SubMenu>
+        )}
+      </Menu>
+    </>
+  );
+};
 
 export default TopNav;
