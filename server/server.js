30ce2c3c27a7c1671e647a78a56cdceb93780927
parisadikshit <2018bec034@sggs.ac.in>
2023-02-06 02:14:52 +0530

    CSRF protection

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const morgan = require('morgan');
 import cors from 'cors';
 import mongoose from 'mongoose';
 require('dotenv').config();
+import cookieParser from 'cookie-parser';
 const userRouter = require('./routes/auth')
+import csrf from 'csurf';
 
 
+const csrfProrection = csrf({cookie:true})
 // db connection 
 mongoose.connect(process.env.DATABASE).then(()=> console.log('##DB Connected##'))
 .catch((e) => console.log(e))
@@ -24,6 +27,13 @@ app.use(morgan('dev'));
 
 // app routes
 app.use(userRouter)
+app.use(cookieParser());
+
+// csrf
+app.use(csrfProrection);
+app.get('/api/csrf-token',(req,res)=>{
+    res.json({csrfToken:req.csrfToken()})
+})
 
 app.listen(port, ()=>{
     console.log(`server is running on ${port}`)
